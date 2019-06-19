6cbc36552cfc62a614d043720012b338682265bf
Jesp <jstriker9peru@gmail.com>
2019-06-19 15:33:00 -0400

    Implemented redux forms

--- components/SignIn/SignInModal.js
+++ components/SignIn/SignInModal.js
@@ -9,9 +9,56 @@ import Grid from '@material-ui/core/Grid';
 import Modal from '@material-ui/core/Modal';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import { Field, reduxForm } from 'redux-form';
 import './SignInModal.scss';
 
-const SignInModal = () => {
+const renderTextField = (
+    { input, label, type, meta: { touched, error } }
+  ) => {
+    return (
+    <TextField
+      variant="outlined"
+      required
+      fullWidth
+      id={input.name}   
+      label={label}
+      error={touched && error}
+      type={type}
+      {...input}
+    />
+)};
+
+const renderCheckbox = ({ input, label }) => (
+    <FormControlLabel
+        control={<Checkbox value="remember" color="primary" 
+        onCheck={input.onChange} />}
+        label={label}
+        onChange={input.onChange}
+        checked={input.value ? true : false}
+    />
+);
+
+const validate = values => {
+    const errors = {};
+    const requiredFields = [
+     'email',
+     'password'
+    ];
+    requiredFields.forEach(field => {
+     if (!values[field]) {
+       errors[field] = 'Required';
+     }
+    });
+    if (
+     values.email &&
+     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+     errors.email = 'Invalid email address';
+    }
+    return errors;
+ }
+
+const SignInModal = ({ invalid, handleSubmit, submitting }) => {
     const [open, setOpen] = React.useState(true);
 
     const handleOpen = () => {
@@ -30,68 +77,56 @@ const SignInModal = () => {
             open={open}
             onClose={handleClose}
          >
-        <div className="sign-in-container">
-            <div className="sign-in">
-                <Avatar className='lock'>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <form className='sign-in-form' noValidate>
-                    <Grid className="grid" container spacing={2}>
-                        <Grid className="grid-item" item xs={12} sm={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
+            <div className="sign-in-container">
+                <div className="sign-in">
+                    <Avatar className='lock'>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className='sign-in-form' onSubmit={handleSubmit}>
+                        <Grid className="grid" container spacing={0}>
+                            <Grid className="grid-item" item xs={12} sm={12}>
+                                <Field name="email" label="Email Address" component={renderTextField} type="email" />
+                            </Grid>
+                            <Grid className="grid-item" item xs={12} sm={12}>
+                                <Field name="password" label="Password" component={renderTextField} type="password" />
+                            </Grid>
+                            <Grid className="remember" item xs={12} sm={12}>
+                                <Field name="remember me" component={renderCheckbox} type="checkbox" label="Remember me" />
+                                {/* <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary" />}
+                                    label="Remember me"
+                                /> */}
+                            </Grid>
+                            <Grid className="grid-item" item xs={12} sm={12}>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className="submit-btn"
+                                    disabled={invalid || submitting}
+                                >
+                                    Sign In
+                                </Button>
+                            </Grid>
+                            <Grid className="grid-item" item>
+                                <Link href="#" variant="body2">
+                                    {"No account yet? Sign Up"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                        <Grid className="grid-item" item xs={12} sm={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </Grid>
-                        <Grid className="remember" item xs={12} sm={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                        </Grid>
-                        <Grid className="grid-item" item xs={12} sm={12}>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className="submit-btn"
-                            >
-                                Sign In
-                            </Button>
-                        </Grid>
-                        <Grid className="grid-item" item>
-                            <Link href="#" variant="body2">
-                                {"Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>   
+                    </form>   
+                </div>
             </div>
-        </div>
-      </Modal>
+        </Modal>
     )
 }
 
-export default SignInModal;
+export default reduxForm({
+    form: 'SignIn',
+    validate,
+    onSubmit: (values) => console.log(values)
+})(SignInModal);
--- components/SignUp/SignUpModal.js
+++ components/SignUp/SignUpModal.js
@@ -2,18 +2,53 @@ import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Modal from '@material-ui/core/Modal';
-// import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-// import Container from '@material-ui/core/Container';
+import { Field, reduxForm } from 'redux-form';
 import './SignUpModal.scss';
 
-const SignUpModal = () => {
+const renderTextField = (
+    { input, label, type, meta: { touched, error } }
+  ) => {
+    return (
+    <TextField
+      variant="outlined"
+      required
+      fullWidth
+      id={input.name}   
+      label={label}
+      error={touched && error}
+      type={type}
+      {...input}
+    />
+)};
+
+const validate = values => {
+    const errors = {};
+    const requiredFields = [
+     'firstName',
+     'lastName',
+     'email',
+     'password'
+    ];
+    requiredFields.forEach(field => {
+     if (!values[field]) {
+       errors[field] = true;
+     }
+    });
+    if (
+     values.email &&
+     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+     errors.email = true;
+    }
+    return errors;
+ }
+
+const SignUpModal = ({ invalid, handleSubmit, submitting }) => {
     const [open, setOpen] = React.useState(true);
 
     const handleOpen = () => {
@@ -32,87 +67,54 @@ const SignUpModal = () => {
             open={open}
             onClose={handleClose}
          >
-        <div className="sign-in-container">
-            <div className="sign-in">
-                <Avatar className='lock'>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign Up
-                </Typography>
-                <form className='sign-in-form' noValidate>
-                    <Grid className="grid" container spacing={2}>
-                        <Grid className="grid-item" item xs={12} sm={6}>
-                        <TextField
-                            autoComplete="fname"
-                            name="firstName"
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="firstName"
-                            label="First Name"
-                            autoFocus
-                        />
+            <div className="sign-in-container">
+                <div className="sign-in">
+                    <Avatar className='lock'>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign Up
+                    </Typography>
+                    <form className='sign-in-form'>
+                        <Grid className="grid" container spacing={0}>
+                            <Grid className="grid-item" item xs={12} sm={6}>
+                                <Field name="firstName" label="First Name" component={renderTextField} type="text" />
+                            </Grid>
+                            <Grid className="grid-item" item xs={12} sm={6}>
+                                <Field name="lastName" label="Last Name" component={renderTextField} type="text" />    
+                            </Grid>
+                            <Grid className="grid-item" item xs={12}>
+                                <Field name="email" label="Email Address" component={renderTextField} type="email" />
+                            </Grid>
+                            <Grid className="grid-item bottom" item xs={12}>
+                                <Field name="password" label="Password" component={renderTextField} type="password" />
+                            </Grid>
+                            <Grid className="grid-item" item xs={12}>    
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className="submit-btn"
+                                    disabled={invalid || submitting}
+                                >
+                                    Sign Up
+                                </Button>
+                            </Grid>
+                            <Grid className="grid-item" item>
+                                <Link href="#" variant="body2">
+                                    {"Already have an account? Sign In"}
+                                </Link>
+                            </Grid>
                         </Grid>
-
-                        <Grid className="grid-item" item xs={12} sm={6}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="lastName"
-                            label="Last Name"
-                            name="lastName"
-                            autoComplete="lname"
-                        />
-                        </Grid>
-
-                        <Grid className="grid-item" item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                        />
-                        </Grid>
-
-                        <Grid className="grid-item bottom" item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        </Grid>
-                        <Grid className="grid-item" item xs={12}>    
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className="submit-btn"
-                            >
-                                Sign Up
-                            </Button>
-                        </Grid>
-                        <Grid className="grid-item" item>
-                            <Link href="#" variant="body2">
-                                {"Already have an account? Sign In"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>   
+                    </form>   
+                </div>
             </div>
-        </div>
-      </Modal>
+         </Modal>
     )
 }
 
-export default SignUpModal;
+export default reduxForm({
+    form: 'SignUp',
+    validate
+})(SignUpModal);
