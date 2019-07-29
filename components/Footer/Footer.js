8ee95db58962886039cde7adfa73904d0831dc69
Jesp <jstriker9peru@gmail.com>
2019-07-28 21:19:16 -0400

    Logo credit

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,8 +5,21 @@ const Footer = () => {
   return (
     <section className="footer-section">
       <div className="content">
-        <p>Movie data powered by</p>
-        <img className="TMDB-logo" alt="TMDB Logo" src="/static/TMDBLogo.svg" />
+        <div className="content-box">
+          <div className="tmdb-tag">
+            <p>Movie data powered by</p>
+            <img className="TMDB-logo" alt="TMDB Logo" src="/static/TMDBLogo.svg" />
+          </div>
+          <div className="logo-tag">
+            Logo made with{" "}
+            <a
+              href="https://www.designevo.com/en/"
+              title="Free Online Logo Maker"
+            >
+              DesignEvo
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
