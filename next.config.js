919cd8b42981a5b08417550951dbd001578df8f4
Jesp <jstriker9peru@gmail.com>
2019-07-29 22:49:43 -0400

    next config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,43 @@ const webpack = require("webpack");
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS(
+const withAssetRelocator = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options
+
+      if (isServer) {
+        config.node = Object.assign({}, config.node, {
+          __dirname: false,
+          __filename: false,
+        })
+
+        config.module.rules.unshift({
+          test: /\.(m?js|node)$/,
+          parser: { amd: false },
+          use: {
+            loader: '@zeit/webpack-asset-relocator-loader',
+            options: {
+              outputAssetBase: 'assets',
+              existingAssetNames: [],
+              wrapperCompatibility: true,
+              escapeNonAnalyzableRequires: true,
+            },
+          },
+        })
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+      return config
+    },
+  })
+}
+
+module.exports = withAssetRelocator(withCSS(
   withSass({
+    target: 'serverless',
     webpack: config => {
       const env = Object.keys(process.env).reduce((acc, curr) => {
         acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
@@ -15,4 +50,4 @@ module.exports = withCSS(
       return config;
     }
   })
-);
+));
